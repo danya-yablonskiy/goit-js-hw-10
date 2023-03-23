7768849462f8f71c8adb885c9b5602dc660856e5
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-03-23 13:44:23 +0200

    fetchCountries.js

--- src/function/fetchCountries.js
+++ src/function/fetchCountries.js
@@ -1,4 +1,3 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export function fetchCountries(name){
    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
    .then(response => {
@@ -6,14 +5,5 @@ export function fetchCountries(name){
        throw new Error();
    }
      return response.json();
-     
    })
-   .then(counrties => {
-     clearInput();
-     inputChecking(counrties)
-   })
-   .catch(() => {
-     Notify.failure("Oops, there is no country with that name");
-     clearInput();
-   })
-   }
+}
